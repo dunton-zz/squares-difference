99ef31df3fa7e55e601c8855a0756b3551e7bb37
Ryan Dunton <ryandunton1@gmail.com>
2018-07-07 16:38:53 -0400

    changed from class to functional component

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import 'styles/datadisplay.css';
 
-class DataDisplay extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-  render() {
-    if (this.props.data.value) {
-      return (
-        <div className="row">
-          <div className="col-md-12 data">
-            <h4>The difference of your request is: {this.props.data.value}</h4>
-            <p>Sent at: {this.props.data.datetime}</p>
-            <p>Number requested: {this.props.data.number}</p>
-          </div>
+function DataDisplay(props) {
+  if (props.data.value) {
+    return (
+      <div className="row">
+        <div className="col-md-12 data">
+          <h4>The difference of your request is: {props.data.value}</h4>
+          <p>Sent at: {props.data.datetime}</p>
+          <p>Number requested: {props.data.number}</p>
         </div>
-      )
-    }
-    else {
-      return null;
-    }
+      </div>
+    )
+  }
+  else {
+    return null;
   }
 }
 
