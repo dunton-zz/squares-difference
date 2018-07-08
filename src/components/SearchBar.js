adbed25fa053dc026f8c50daca5ac62188b672f8
Ryan Dunton <ryandunton1@gmail.com>
2018-07-07 21:19:03 -0400

    very basic error handling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,16 +6,36 @@ class SearchBar extends Component {
     super(props);
 
     this.state = {
-      number: ''
+      number: '',
+      error: false
     }
 
     // bind functions
     this.onSubmit = this.onSubmit.bind(this);
     this.updateInput = this.updateInput.bind(this);
+    this.validateInput = this.validateInput.bind(this);
   }
 
   updateInput(e){
     this.setState({number: e.target.value})
+  }
+
+  validateInput(number) {
+    // check if number is between 0 and 100
+    if (number < 0 || number > 100) { // TODO better error handling
+      alert('Please enter a number between 0 and 100');
+      this.setState({number: '', error: true});
+      return false;
+    }
+
+    // check if a number
+    if (Number.isInteger(number)) {
+      alert('Please enter a whole number between 0 and 100');
+      this.setState({number: '', error: true});
+      return false;
+    }
+
+    return true;
   }
 
   onSubmit(e) {
@@ -25,16 +45,14 @@ class SearchBar extends Component {
     // grab number value
     let number = this.state.number;
 
-    // check if number is between 0 and 100
-    if (number < 0 || number > 100) { // TODO better error handling
-      alert('Please enter a number between 0 and 100');
-    }
+
 
     // reset state so nothing displays in input
     this.setState({number: ''})
-    // call onFormSubmit
-    this.props.onFormSubmit(number);
-
+    // call onFormSubmit if no errors
+    if (this.validateInput(number)) {
+      this.props.onFormSubmit(number);
+    }
   }
   render() {
     return(
@@ -42,7 +60,7 @@ class SearchBar extends Component {
         <div className="col-md-12">
           <h3>Input a number to find the sum of the squares of the first n natural numbers and the square of the sum of the same first n natural numbers</h3>
           <form onSubmit={this.onSubmit}>
-            <input type="number" ref="number" value={this.state.number} onChange={this.updateInput} className="form-control" placeholder="Input a number!"></input>
+            <input type="number" ref="number" value={this.state.number} onChange={this.updateInput} className="form-control {error}" placeholder="Input a number!"></input>
           </form>
         </div>
       </div>
